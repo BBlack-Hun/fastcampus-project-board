78437367c588c5a338ce67b6b129f2d0bc7d3efe
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-19 01:33:44 +0900

    #5 - Querydsl 지원을 이용해서 api 검색 옵션 추가
    `QuerydslPredicateExecutor`,
    `QuerydslBlinderCustomizer` 를 이용하여 간단하게 개발

--- src/main/java/com/example/projectboard/api/Article/repository/ArticleRepository.java
+++ src/main/java/com/example/projectboard/api/Article/repository/ArticleRepository.java
@@ -1,10 +1,29 @@
 package com.example.projectboard.api.Article.repository;
 
 import com.example.projectboard.api.Article.entity.Article;
+import com.example.projectboard.api.Article.entity.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.stereotype.Repository;
 
 @RepositoryRestResource
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleRepository extends
+        JpaRepository<Article, Long>,
+        QuerydslPredicateExecutor<Article>,
+        QuerydslBinderCustomizer<QArticle>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QArticle root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title, root.content, root.hashTag, root.createdAt, root.createdBy);
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // %를 자동으로 넣어줌.
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.hashTag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
