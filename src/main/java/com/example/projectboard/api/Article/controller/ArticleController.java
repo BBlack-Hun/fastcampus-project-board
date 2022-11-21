56edc254d18bb8497d569cbafb1aa0903c19b342
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-21 20:00:47 +0900

    #16 - 게시판 뷰 기본 골격 작성
    컨트롤로 핸들러 메소드로 테스트를 통과하게 열어줌
    뷰 적절히 뼈대 잡음
    테스트에서는 media type 검사에서 부가적인 옵션이 들어갈 수 있음을 확인했기 때문에 적절히 테스트 메소드 수정

--- src/main/java/com/example/projectboard/api/Article/controller/ArticleController.java
+++ src/main/java/com/example/projectboard/api/Article/controller/ArticleController.java
@@ -1,9 +1,20 @@
 package com.example.projectboard.api.Article.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
 
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
+
+    @GetMapping
+    public String articles(ModelMap map) {
+        map.addAttribute("articles", new ArrayList<>());
+
+        return "articles/index";
+    }
 }
