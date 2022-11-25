e48a8cf053128a20bfda311985e535cffa750334
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-26 00:52:34 +0900

    #21 - 루트 경로를 게시판 페이지로 forward 하도록 컨트롤러 구현 MainController 를 만들어서 기본 페이지가 게시판 페이지가 되도록 함 강의 중에서는 redirect 로 접근했는데 단순 조회 접근이고, url 루트 경로를 유지한 채로 게시판 페이지를 대표로 만들고 싶은 상황이므로 forward 가 더 적절해보임

---
+++ src/main/java/com/example/projectboard/api/home/controller/MainController.java
@@ -0,0 +1,13 @@
+package com.example.projectboard.api.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/articles";
+    }
+}
