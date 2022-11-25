7b62ebc20f417c128b283addbb62e6c703482078
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-26 00:51:43 +0900

    #21 - 게시글 서비스 로직의 테스트와 골격 잡기 다음의 기능을 테스트로 표현
    * 게시글 검색하면 게시글 리스트 반환
    * 게시글을 ID로 조회
    * 게시글 생성
    * 게시글 수정
    * 게시글 삭제
    
    필요한 dto 와 enum을 함께 작성했는데,
    추후에 수정할 수도 있음

---
+++ src/main/java/com/example/projectboard/api/Article/payload/ArticleDto.java
@@ -0,0 +1,53 @@
+package com.example.projectboard.api.Article.payload;
+
+import com.example.projectboard.api.Article.entity.Article;
+import com.example.projectboard.api.User.payload.UserAccountDto;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * A DTO for the {@link com.example.projectboard.api.Article.entity.Article} entity
+ */
+@Data
+public class ArticleDto{
+
+    private final Long id;
+    private final UserAccountDto userAccountDto;
+    private final String title;
+    private final String content;
+    private final String hashTag;
+    private final LocalDateTime createdAt;
+    private final String createdBy;
+    private final LocalDateTime modifiedAt;
+    private final String modifiedBy;
+
+    public static ArticleDto of(Long id, UserAccountDto userAccountDto, String title, String content, String hashTag,
+                                LocalDateTime createdAt, String createdBy, LocalDateTime modifiedAt, String modifiedBy) {
+        return new ArticleDto(id, userAccountDto, title, content, hashTag, createdAt, createdBy, modifiedAt, modifiedBy);
+    }
+
+    public static ArticleDto from(Article entity) {
+        return new ArticleDto(
+            entity.getId(),
+            UserAccountDto.from(entity.getUserAccount()),
+            entity.getTitle(),
+            entity.getContent(),
+            entity.getHashTag(),
+            entity.getCreatedAt(),
+            entity.getCreatedBy(),
+            entity.getModifiedAt(),
+            entity.getModifiedBy()
+        );
+    }
+
+    public Article toEntity() {
+        return Article.of(
+                userAccountDto.toEntity(),
+                title,
+                content,
+                hashTag
+        );
+    }
+
+}
