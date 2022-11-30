cf7e061b0dd7c8f30f1ea84e28560ff7e03be172
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-30 14:48:03 +0900

    #42 - 테스트용 TestObject 생성 및 <form> 데이터 전달 테스트용 인코더 추가
    테스트를 위해 TestObject 를 생성하여 테스트를 진행함.
    form data 는 api 와는 다른 형태로 직렬화하여 사용해야 한다.
    객체를 규약에 맞게 form data 포맷의 문자열로 바꿔주는
    유틸리티를 테스트 전용으로 만들고, 테스트도 함께 작성
    이게 없으면 `post().param()` 형태로 값을 넣을 수도 있지만
    표현이 실제 post request 전송과 꼭 맞지 않는 듯 하고,
    객체가 아니라 파라미터 단위로 넣어줘야 하므로 불편함.

---
+++ src/test/java/com/example/projectboard/util/TestObject.java
@@ -0,0 +1,35 @@
+package com.example.projectboard.util;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class TestObject {
+
+    private final String str;
+    private final String listStr1;
+    private final String listStr2;
+    private final String nullStr;
+    private final Integer number;
+    private final Double floatingNumber;
+    private final Boolean bool;
+    private final BigDecimal bigDecimal;
+    private final FormDataEncoderTest.TestEnum testEnum;
+
+    public TestObject(String str, String listStr1, String listStr2, String nullStr, Integer number, Double floatingNumber, Boolean bool, BigDecimal bigDecimal, FormDataEncoderTest.TestEnum testEnum) {
+        this.str = str;
+        this.listStr1 = listStr1;
+        this.listStr2 = listStr2;
+        this.nullStr = nullStr;
+        this.number = number;
+        this.floatingNumber = floatingNumber;
+        this.bool = bool;
+        this.bigDecimal = bigDecimal;
+        this.testEnum = testEnum;
+    }
+
+    public TestObject of(String str, String listStr1, String listStr2, String nullStr, Integer number, Double floatingNumber, Boolean bool, BigDecimal bigDecimal, FormDataEncoderTest.TestEnum testEnum) {
+        return new TestObject(str, listStr1, listStr2, nullStr, number, floatingNumber, bool, bigDecimal, testEnum);
+    }
+}
