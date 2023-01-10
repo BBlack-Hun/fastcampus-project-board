43eecc5bd4469f5e03285e884dcf899fc86c84b1
BBlack-Hun <h1103j@kangwon.ac.kr>
2023-01-10 09:38:57 +0900

    #62 - AuthControllerTest: Void.class 표현 개선
    원래 이 자리에는 아무 클래스가 아니라 빈이 들어가야 한다.
    `Void`는 스프링 빈이 아니므로 정확한 표현이 아니고
    일부 개발 환경에서 에러를 유발하는 것을 확인
    이에 테스트 전용 빈을 제대로 만들어서 넣는 방법으로 개선.
    테스트 빈은 아무 일도 하지 않는 것이 목적이기 때문에
    안에 내용을 가지고 있지 않다.
    다른 테스트에서 재활용할 가능성이 적은 점을 고려해
    inner class 로 작성.

--- src/test/java/com/example/projectboard/api/Article/controller/AuthControllerTest.java
+++ src/test/java/com/example/projectboard/api/Article/controller/AuthControllerTest.java
@@ -1,24 +1,32 @@
 package com.example.projectboard.api.Article.controller;
 
+import com.example.projectboard.api.Article.service.ArticleService;
+import com.example.projectboard.api.Common.service.PaginationService;
 import com.example.projectboard.core.util.config.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest(Void.class)
+@WebMvcTest(AuthControllerTest.EmptyController.class)
 public class AuthControllerTest {
 
     private MockMvc mockMvc;
+
+    @MockBean private ArticleService articleService;
+    @MockBean private PaginationService paginationService;
 
     AuthControllerTest(@Autowired MockMvc mockMvc) {
         this.mockMvc = mockMvc;
@@ -32,5 +40,10 @@ public class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andDo(MockMvcResultHandlers.print());
+        then(articleService).shouldHaveNoInteractions();
+        then(paginationService).shouldHaveNoInteractions();
     }
+
+    @TestComponent
+    static class EmptyController {}
 }
